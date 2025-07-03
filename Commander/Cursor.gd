21a7dcfa3c7077f4671c6fe41875792bc09fd0a1
Arnaud Jomard <arnaud.jomard@outlook.fr>
2025-07-03 21:11:20 +0200

    Cursor v1 working well

--- Commander/Cursor.gd
+++ Commander/Cursor.gd
@@ -6,9 +6,21 @@ extends Node2D
 		return radius
 	set(nRadius):
 		radius = nRadius
-		
+@export var distanceToHolder : float = 10
+
+@onready var playerId : int = 0
+@onready var pos : Vector2
+
 func _ready():
 	queue_redraw()
 	
+func _input(event: InputEvent) -> void:
+	position = Vector2.ZERO
+	if Input.get_connected_joypads().has(playerId):
+		var horizontalDirection = Input.get_joy_axis(playerId, JOY_AXIS_RIGHT_X)
+		var verticalDirection = Input.get_joy_axis(playerId, JOY_AXIS_RIGHT_Y)
+		if (Vector2(horizontalDirection, verticalDirection).length() > 0.2):
+			position = Vector2(horizontalDirection, verticalDirection).normalized() * distanceToHolder
+
 func _draw():
-	draw_circle(Vector2(0, 0), radius, color)
+	draw_circle(Vector2.ZERO, radius, color)
