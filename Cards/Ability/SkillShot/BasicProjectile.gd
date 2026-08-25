8fdd759e39e1ab7c453594ddeef8089504687bbb
Arnaud Jomard <arnaud.jomard@outlook.fr>
2026-08-25 15:52:53 +0900

    cleanup ignored files

--- Cards/Ability/SkillShot/BasicProjectile.gd
+++ Cards/Ability/SkillShot/BasicProjectile.gd
@@ -6,24 +6,40 @@ extends Node2D
 @export var speed : float = 3000
 @export var damage : float = 10 :
 	set(nDamage): damage = nDamage
+	
 @onready var dir : Vector2
 @onready var collisionshape : CircleShape2D = $Hitbox/CollisionShape2D.shape
 @onready var hitbox : Hitbox2D = $Hitbox
+@onready var raycast : RayCast2D = $RayCast2D
 
 func _ready():
 	set_as_top_level(true)
 	queue_redraw()
-	hitbox.connect("triggerArea", hit)
+	hitbox.triggerArea.connect(hit)
+	hitbox.triggerBody.connect(vanish)
 	
 func _physics_process(delta: float) -> void:
-	position += speed * dir * delta
+	raycast.target_position = dir * (speed * delta + 5.0)
+	raycast.force_raycast_update()
+	
+	if raycast.is_colliding():
+		var impact_point := raycast.get_collision_point()
+		global_position = impact_point
+		vanish(raycast.get_collider())
+		return
+		
+	global_position += speed * dir * delta
 	
 func _draw():
 	draw_circle(Vector2.ZERO, radius, color)
 
 func hit(collsionEntity: Node2D) -> void:
 	collsionEntity.takeHit.emit(damage)
+	vanish(collsionEntity)
 	
+func vanish(_collsionEntity: Node2D) -> void:
+	call_deferred("queue_free")
+
 func setRadius(nRadius: float) -> void:
 	radius = nRadius
 	if (collisionshape != null):
--- Utility/CustomNode/Hitbox2D.gd
+++ Utility/CustomNode/Hitbox2D.gd
@@ -2,10 +2,10 @@ class_name Hitbox2D
 extends Area2D
 
 signal triggerArea(area)
+signal triggerBody(body)
 
 func _on_area_entered(area: Area2D) -> void:
-	emit_signal("triggerArea", area)
-	owner.queue_free()
-
+	triggerArea.emit(area)
+	
 func _on_body_entered(body: Node2D) -> void:
-	owner.queue_free()
+	triggerBody.emit(body)
